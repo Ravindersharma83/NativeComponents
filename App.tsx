b63c690dd0000e72f036a6b11900ba676f16107c
ravindersharma83 <ravinder200198@gmail.com>
2023-07-20 18:01:43 +0530

    React Native Cli Components Examples

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+      <Text>Welcome</Text>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
