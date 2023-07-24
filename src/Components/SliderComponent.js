3941182fa63dc54ed38a081238bad825f433b083
ravindersharma83 <ravinder200198@gmail.com>
2023-07-24 18:20:14 +0530

    24- Slider Example 2

--- src/Components/SliderComponent.js
+++ src/Components/SliderComponent.js
@@ -1,22 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
+// import { StyleSheet, Text, View } from 'react-native'
+// import React, { useState } from 'react'
+// import Slider from '@react-native-community/slider'
+
+// const SliderComponent = () => {
+//     const [range,setRange] = useState(0);
+//   return (
+//     <View style={styles.container}>
+//         <Text style={{opacity:range}}>Visibility</Text>
+//         <Text>{Math.floor(range * 100)}</Text>
+    //   <Slider
+    //     style={{width: 300, height: 40}}
+    //     minimumValue={0}
+    //     maximumValue={1}
+    //     // minimumTrackTintColor="#FFFFFF"
+    //     // maximumTrackTintColor="#000000"
+    //     thumbTintColor='pink'
+    //     onValueChange={(value)=>setRange(value)}
+    //   />
+//     </View>
+//   )
+// }
+
+// export default SliderComponent
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems:'center'
+//       },
+// })
+
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
-import Slider from '@react-native-community/slider'
+import Slider from '@react-native-community/slider';
 
 const SliderComponent = () => {
-    const [range,setRange] = useState(0);
+    const [name,setName] = useState(null);
+    const [age,setAge] = useState(null);
   return (
     <View style={styles.container}>
-        <Text style={{opacity:range}}>Visibility</Text>
-        <Text>{Math.floor(range * 100)}</Text>
+      <TextInput style={styles.input} placeholder='Enter Name' value={name} onChangeText={(val)=>setName(val)} />
+      <Text>{Math.floor(age)}</Text>
+      <Text style={{marginVertical:20}}>Select Date</Text>
       <Slider
         style={{width: 300, height: 40}}
-        minimumValue={0}
-        maximumValue={1}
-        // minimumTrackTintColor="#FFFFFF"
-        // maximumTrackTintColor="#000000"
+        minimumValue={18}
+        maximumValue={100}
         thumbTintColor='pink'
-        onValueChange={(value)=>setRange(value)}
+        onValueChange={(value)=>setAge(value)}
       />
+      <Button title='Submit' onPress={()=>Alert.alert(`welcome ${name}. Your age is ${Math.floor(age)}`)} />
     </View>
   )
 }
@@ -24,9 +58,13 @@ const SliderComponent = () => {
 export default SliderComponent
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
+    container:{
+        flex:1,
+        justifyContent:'center',
         alignItems:'center'
-      },
+    },
+    input:{
+        borderWidth:1,
+        width:200
+    }
 })
